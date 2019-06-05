0f1f3599af9df7f71098d5c14cf30c5a4e71bbb0
Alexander Kondra and Jacob Irwin <alexander@>
2019-06-05 13:46:25 -0700

    start form project

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,9 @@
-// Business (or back-end) logic:
 
-var add = function(number1, number2) {
-  return number1 + number2;
-};
+var food = $("#food").val();
 
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
+var color = $("input:radio[name=color]:checked").val();
+console.log(color);
 
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
+var favoriteColor = $("#color").val();
 
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
-// User interface (or front-end) logic:
-
-$(document).ready(function() {
-  $("#adding").click(function(event) {
-   event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var resultAdd = add(number1, number2);
-    $("#addOut").text(resultAdd);
-  });
-  $("#substracting").click(function(event) {
-   event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var resultSub = subtract(number1, number2);
-    $("#subOut").text(resultSub);
-  });
-  $("#multiplying").click(function(event) {
-   event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var resultMult = multiply(number1, number2);
-    $("#multOut").text(resultMult);
-  });
-  $("#divide").click(function(event) {
-   event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var resultDiv = divide(number1, number2);
-    $("#divOut").text(resultDiv);
-  });
-});
+var dob = $("#born").val();
