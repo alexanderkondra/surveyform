4651a16356f6a72ebb13ba74ef39e60c726f833f
Alexander Kondra and Jacob Irwin <jacob@>
2019-06-05 15:14:23 -0700

    get stuck on the color wheel and dob section

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
-
-var food = $("#food").val();
-
-var color = $("input:radio[name=color]:checked").val();
-
-var favoriteColor = $("#color").val();
-
-var dob = $("#born").val();
-
 $(document).ready(function() {
     $("#survey form").submit(function(event) {
-      event.preventDefault();
-      survey.
+
+      var food = $("#food").val();
+      $(".food").text(food);
+
+      var sport = $("input:radio[name=sport]:checked").val();
+      $(".sport").text(sport);
+
+      var color = $("#colorWheel").val();
+      $(".color").text(color);
+
+      var dob = $("#born").val();
+      console.log(dob);
+      $(".DOB").text(dob);
+
+
+      var name = $("#name").val();
+      $(".name").text(name);
+
+
+
 
 
     $("#results").show();
 
+    event.preventDefault();
   });
 });
