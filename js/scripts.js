a737b88e57cf527e3edcee9730cdb456608503ca
Alexander Kondra and Jacob Irwin <jacob@>
2019-06-05 10:59:12 -0700

    add buttons and functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -14,12 +16,35 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+// User interface (or front-end) logic:
+
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-  event.preventDefault();
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-  var result = add(number1, number2);
-  $("#output").text(result);
+  $("#adding").click(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var resultAdd = add(number1, number2);
+    $("#addOut").text(resultAdd);
+  });
+  $("#substracting").click(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var resultSub = subtract(number1, number2);
+    $("#subOut").text(resultSub);
+  });
+  $("#multiplying").click(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var resultMult = multiply(number1, number2);
+    $("#multOut").text(resultMult);
+  });
+  $("#divide").click(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var resultDiv = divide(number1, number2);
+    $("#divOut").text(resultDiv);
   });
 });
